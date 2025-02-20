fe15f7862891a45cf7cd90311bc1643d01ea34eb
kimseungbin <luxorienskim@gmail.com>
2025-02-20 13:59:25 +0900

    test: Plan out unit tests for main.ts #43

---
+++ src/main.spec.ts
@@ -0,0 +1,4 @@
+describe('bootstrap', () => {
+	it.todo('should call NestFactory.create with AppModule and call listen with process.env.PORT if set')
+	it.todo('should default to port 3000 if process.env.PORT is not set')
+})
