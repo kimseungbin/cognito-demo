c82bd7c938aeb8bd49e3b7449d11d539e7c03d9a
kimseungbin <luxorienskim@gmail.com>
2025-02-21 13:28:52 +0900

    test: Implement App Module unit testing #55

--- src/app/app.module.spec.ts
+++ src/app/app.module.spec.ts
@@ -1,3 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { ConfigService } from '@nestjs/config'
+import { AppModule } from './app.module'
+
 describe('AppModule', () => {
-	it.todo('should have ConfigService defined.')
+	let module: TestingModule
+
+	beforeAll(async () => {
+		module = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile()
+	})
+
+	it('should have ConfigService defined.', () => {
+		const configService = module.get<ConfigService>(ConfigService)
+		expect(configService).toBeDefined()
+	})
 })
